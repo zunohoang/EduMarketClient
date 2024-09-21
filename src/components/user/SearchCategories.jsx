e440dabf7021786fedd002434d5efc9331c9e5e1
zunohoang <nguyenvanhoang2005nt@gmail.com>
2024-09-22 02:18:21 +0700

    Add: SearchCategories

---
+++ src/components/user/SearchCategories.jsx
@@ -0,0 +1,29 @@
+
+function Categories({ title }) {
+    return (
+        <div className="text-[#2E5E97] bg-[#F3F5F6] text-[13.125px] p-1 rounded-lg shadow-sm">
+            {title}
+        </div>
+    )
+
+}
+
+export default function SearchCategories() {
+    return (
+        <div className="search-categories bg-[#FFFFFF] rounded-xl p-5 mt-8">
+            <h3 className="text-[#355676] text-[15.75px] font-bold">Bạn đang tìm kiếm gì ?</h3>
+            <div className="flex flex-wrap gap-3 mt-3">
+                <Categories title="Lớp học" />
+                <Categories title="Khóa học" />
+                <Categories title="Giáo viên" />
+                <Categories title="Trung tâm" />
+                <Categories title="Tài liệu" />
+                <Categories title="Sự kiện" />
+                <Categories title="Thầy Mạnh Dũng" />
+                <Categories title="Khóa học" />
+                <Categories title="Giáo viên" />
+                <Categories title="Trung tâm" />
+            </div>
+        </div>
+    )
+}
