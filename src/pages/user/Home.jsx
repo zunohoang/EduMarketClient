e750e9964cad3ee151ad96a1d2f805f34673747a
Nam Khuc <khucphuongnam2005@hotmail.com>
2024-09-22 00:41:21 +0700

    MODFIED: image and file name + do sideBar.

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import Bg from '../../assets/banner.jpg'
+import Bg from '../../assets/banner.png'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
     return (
         <div className="mt-6">
-            <img src={Bg} alt="home" className="object-cover w-[700px] rounded-[15px] m-auto" />
+                <img src={Bg} alt="home" className="cursor-pointer object-cover w-[700px] rounded-[15px] m-auto" />
         </div>
     )
 }
