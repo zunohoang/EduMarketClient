826aa0e9890a87e5de1a3c6807e58522398947b8
Nam Khuc <khucphuongnam2005@hotmail.com>
2024-09-23 01:28:14 +0700

    ADD: RightBar - FIX: Overwidth

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -2,19 +2,18 @@ import React from 'react'
 import Bg from '../../assets/banner.png'
 import { Link } from 'react-router-dom'
 import SearchCategories from '../../components/user/SearchCategories'
-import SideBar from '../../components/user/SideBar'
+import LeftBar from '../../components/user/LeftBar'
+import RightBar from '../../components/user/RightBar'
 
 export default function Home() {
     return (
-        <div className="mt-6 gap-12 grid grid-cols-[1fr_2fr_1fr] w-screen ">
-            <SideBar />
+        <div className="mt-6 gap-12 grid grid-cols-[1fr_2fr_1fr] w-full">
+            <LeftBar />
             <div className=''>
                 <img src={Bg} alt="home" className="cursor-pointer object-cover w-full rounded-[15px] m-auto" />
                 <SearchCategories />
             </div>
-            <div className=''>
-                right
-            </div>
+            <RightBar />
         </div>
     )
 }
