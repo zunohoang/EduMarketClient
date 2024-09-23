e6c3e6605fad37d730b540ec3dceaecbf1e0d872
zunohoang <nguyenvanhoang2005nt@gmail.com>
2024-09-24 00:21:24 +0700

    CategoriesCourse

--- src/pages/user/Course.jsx
+++ src/pages/user/Course.jsx
@@ -1,8 +1,9 @@
+import Categories from "../../components/user/Categories/Categories.jsx";
 
 export default function Course() {
     return (
-        <div>
-            Course
+        <div className="pt-[70px]">
+            <Categories />
         </div>
     )
 }
