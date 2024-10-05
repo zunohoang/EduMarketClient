fec74f426e370df196292262fbcf231903b3d170
zunohoang <nguyenvanhoang2005nt@gmail.com>
2024-10-06 06:26:20 +0700

    FIX: CourseList Slide

--- src/components/user/Course/CourseTabs.jsx
+++ src/components/user/Course/CourseTabs.jsx
@@ -1,8 +1,28 @@
 
+function ClassCard({ title, isActive }) {
+    return (
+        <div className="flex items-center justify-center p-6 h-12 cursor-pointer"
+            style={{
+                color: isActive ? "#2D5D90" : "#9CA3AF",
+                borderBottom: isActive ? "2px solid #2D5D90" : "none"
+            }}
+        >
+            <p className="text-lg font-semibold">{title}</p>
+        </div>
+    )
+}
+
 export default function CourseTabs() {
     return (
-        <div className="m-8 w-full">
-            <div className=" h-[1px] w-full my-3 mx-auto bg-[rgba(0,0,0,0.1)]"></div>
+        <div className="mt-8 pl-12 w-full">
+            <div className="flex">
+                <ClassCard title={"Tất cả"} isActive={true} />
+                <ClassCard title={"Lớp 12"} isActive={false} />
+                <ClassCard title={"Lớp 11"} isActive={false} />
+                <ClassCard title={"Lớp 10"} isActive={false} />
+                <ClassCard title={"Lớp 9"} isActive={false} />
+            </div>
+            <div className=" h-[1px] w-full bg-[rgba(0,0,0,0.1)]"></div>
         </div>
     )
 }
--- src/pages/user/Course.jsx
+++ src/pages/user/Course.jsx
@@ -1,12 +1,30 @@
 import Categories from "../../components/user/Categories/Categories.jsx";
 import CourseCatalog from "../../components/user/Course/CourseCatalog.jsx";
+import FilterSidebar from "../../components/user/Course/FilterSidebar.jsx";
 import CourseTabs from "../../components/user/Course/CourseTabs.jsx";
+
+function CourseList() {
+    return (
+        <div className="w-full">
+
+        </div>
+    )
+}
 
 export default function Course() {
     return (
         <div className="pt-[70px] m-3">
             <Categories />
-            <CourseCatalog />
+            <div className="m-6 mt-12">
+                <h3 className="text-[#355676] text-[17.5px] font-bold">DANH MỤC</h3>
+                <div className="flex gap-6">
+                    <FilterSidebar />
+                    <div className="w-full">
+                        <CourseTabs />
+                        <CourseList />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
