08ddbc9609906a67c5a66a850cf702f5508a8aad
Nam Khuc <khucphuongnam2005@hotmail.com>
2024-10-07 15:10:32 +0700

    ADD:News ; ERR: bg-url

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LayoutUser from '../Layouts/LayoutUser';
 import Home from '../pages/user/Home';
 import Course from '../pages/user/Course';
-
+import News from '../pages/user/News'
 export default function AppRoutes() {
     return (
         <BrowserRouter>
@@ -11,7 +11,7 @@ export default function AppRoutes() {
                 <Route path="/" element={<LayoutUser />}>
                     <Route index element={<Home />} />
                     <Route path="courses" element={<Course />} />
-                    <Route path="news" element={<Home />} />
+                    <Route path="news" element={<News />} />
                 </Route>
                 {/* <Route path="/admin" element={<LayoutAdmin />}>
                     <Route index element={<Home />} />
