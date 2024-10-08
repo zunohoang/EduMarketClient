1ceabffa5657c7fb99732449678e6363a6baa47e
zunohoang <nguyenvanhoang2005nt@gmail.com>
2024-10-08 23:57:55 +0700

    ADD: Vercel Config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Chỉ định base URL cho ứng dụng
+  build: {
+    outDir: 'dist' // Đảm bảo thư mục đầu ra khớp với vercel.json
+  }
 })
