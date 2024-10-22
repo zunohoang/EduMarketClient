4ac77c480bb6018333f0ca24e3ddb9a2ee8f670c
Nam Khuc <khucphuongnam2005@gmail.com>
2024-10-23 02:35:38 +0700

    MODIFIED:Admin/Edit&Del

--- src/components/admin/SideBar.jsx
+++ src/components/admin/SideBar.jsx
@@ -3,13 +3,13 @@ import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
 
 export default function SideBar() {
-    const [coursesDisplay, setCoursesDisplay] = useState('hidden');
-    const [studentsDisplay, setStudentsDisplay] = useState('hidden');
-    const [teachersDisplay, setTeachersDisplay] = useState('hidden');
-    const [settingsDisplay, setSettingsDisplay] = useState('hidden');
+    const [coursesDisplay, setCoursesDisplay] = useState(false);
+    const [studentsDisplay, setStudentsDisplay] = useState(false);
+    const [teachersDisplay, setTeachersDisplay] = useState(false);
+    const [settingsDisplay, setSettingsDisplay] = useState(false);
 
-    const toggleDisplay = (section, setSectionDisplay) => {
-        setSectionDisplay(section === 'hidden' ? '' : 'hidden');
+    const toggleDisplay = (currentDisplay, setDisplay) => {
+        setDisplay(!currentDisplay);
     };
 
     return (
@@ -28,7 +28,7 @@ export default function SideBar() {
                     <div className='p-3 hover:text-[#2D5D90] hover:bg-white' onClick={() => toggleDisplay(coursesDisplay, setCoursesDisplay)}>
                         <h1 className='text-xl'>Quản lí khóa học</h1>
                     </div>
-                    <ul className={`${coursesDisplay} pl-10 text-md flex flex-col`}>
+                    <ul className={`pl-10 text-md flex flex-col transition-all duration-500 ease-in-out overflow-hidden ${coursesDisplay ? 'max-h-40' : 'max-h-0'}`}>
                         <Link to={'./courses/list'} className='hover:text-[#2D5D90] hover:bg-white'>Danh sách khóa học</Link>
                         <Link to={'./courses/add'} className='hover:text-[#2D5D90] hover:bg-white'>Tạo khóa học mới</Link>
                     </ul>
@@ -38,7 +38,7 @@ export default function SideBar() {
                     <div className='p-3 hover:text-[#2D5D90] hover:bg-white' onClick={() => toggleDisplay(studentsDisplay, setStudentsDisplay)}>
                         <h1 className='text-xl'>Quản lí người dùng</h1>
                     </div>
-                    <ul className={`${studentsDisplay} pl-10 text-md flex flex-col`}>
+                    <ul className={`pl-10 text-md flex flex-col transition-all duration-500 ease-in-out overflow-hidden ${studentsDisplay ? 'max-h-20' : 'max-h-0'}`}>
                         <Link to={'./students/list'} className='hover:text-[#2D5D90] hover:bg-white'>Danh sách người dùng</Link>
                     </ul>
                 </NavLink>
@@ -47,7 +47,7 @@ export default function SideBar() {
                     <div className='p-3 hover:text-[#2D5D90] hover:bg-white' onClick={() => toggleDisplay(teachersDisplay, setTeachersDisplay)}>
                         <h1 className='text-xl'>Quản lí giảng viên</h1>
                     </div>
-                    <ul className={`${teachersDisplay} pl-10 text-md flex flex-col`}>
+                    <ul className={`pl-10 text-md flex flex-col transition-all duration-500 ease-in-out overflow-hidden ${teachersDisplay ? 'max-h-40' : 'max-h-0'}`}>
                         <Link to={'./teachers/list'} className='hover:text-[#2D5D90] hover:bg-white'>Danh sách giảng viên</Link>
                         <Link to={'./teachers/add'} className='hover:text-[#2D5D90] hover:bg-white'>Cấp quyền giảng viên</Link>
                     </ul>
@@ -57,7 +57,7 @@ export default function SideBar() {
                     <div className='p-3 hover:text-[#2D5D90] hover:bg-white' onClick={() => toggleDisplay(settingsDisplay, setSettingsDisplay)}>
                         <h1 className='text-xl'>Cài đặt</h1>
                     </div>
-                    <ul className={`${settingsDisplay} pl-10 text-md`}>
+                    <ul className={`pl-10 text-md flex flex-col transition-all duration-500 ease-in-out overflow-hidden ${settingsDisplay ? 'max-h-20' : 'max-h-0'}`}>
                         <li className='hover:text-[#2D5D90] hover:bg-white'>Chung</li>
                         <li className='hover:text-[#2D5D90] hover:bg-white'>Bảo mật</li>
                     </ul>
