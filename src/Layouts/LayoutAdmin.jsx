62119f079fc34d3f917634377d69fe110b6190e8
zunohoang <nguyenvanhoang2005nt@gmail.com>
2024-11-06 13:03:12 +0700

    FEATURE: admin

--- src/Layouts/LayoutAdmin.jsx
+++ src/Layouts/LayoutAdmin.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import SideBar from "../components/admin/SideBar";
 import Cache from "../components/Cache";
 import { useEffect, useRef, useState } from "react";
 import { Menu } from "lucide-react";
+import { LogOut } from 'lucide-react';
+import Cookies from "js-cookie";
+import { House } from 'lucide-react';
 
 export default function LayoutAdmin() {
     const [isSideBarOpen, setIsSideBarOpen] = useState(false);
     const sidebarRef = useRef(null);
 
-    // Đóng sidebar khi bấm ra ngoài
     useEffect(() => {
         function handleClickOutside(event) {
             if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
@@ -21,29 +23,53 @@ export default function LayoutAdmin() {
         };
     }, []);
 
+
+    useEffect(() => {
+        if (!Cookies.get("accesstoken")) {
+            window.location.href = "/login";
+        }
+    }, []);
+
+    const handleLogout = () => {
+        Cookies.remove("accesstoken");
+        Cookies.remove("role");
+        Cookies.remove("fullName");
+        Cookies.remove("email");
+        Cookies.remove("id");
+        Cookies.remove("username");
+        window.location.href = "/login";
+    }
+
     return (
-        <div className="w-full">
-            <div
-                ref={sidebarRef}
-                className={`fixed lg:w-[240px] lg:block z-10 transition-all duration-500 ease-in-out transform lg:transform-none ${isSideBarOpen ? 'w-[240px] translate-x-0' : '-translate-x-full'
-                    }`}
-            >
-                <SideBar />
-            </div>
-            <div className="bg-[#F3F4F5] lg:ml-[240px]">
-                <div className="bg-white shadow-md p-4 flex justify-between items-center fixed z-0 w-full top-0 left-0">
-                    <div onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
-                        <Menu />
+        Cookies.get("accesstoken") === undefined || Cookies.get("role") === "STUDENT" ? <div>Không đủ quyền</div> :
+            <div className="w-full">
+                <div
+                    ref={sidebarRef}
+                    className={`fixed lg:w-[240px] lg:block z-10 transition-all duration-500 ease-in-out transform lg:transform-none ${isSideBarOpen ? 'w-[240px] translate-x-0' : '-translate-x-full'
+                        }`}
+                >
+                    <SideBar />
+                </div>
+                <div className="bg-[#F3F4F5] lg:ml-[240px]">
+                    <div className="bg-white shadow-md p-4 flex justify-between items-center fixed z-0 w-full top-0 left-0">
+                        <div onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
+                            <Menu />
+                        </div>
+                        <div className="flex items-center box-border gap-2">
+                            <div className="text-sm font-semibold">{Cookies.get("role")}, {Cookies.get("fullName")}</div>
+                            <Link to="/" className="bg-sky-600 hover:bg-blue-400 duration-500 flex items-center p-1 gap-2 text-white  rounded-md">
+                                <House size={16} />
+                            </Link>
+                            <button onClick={handleLogout} className="bg-red-700 hover:bg-red-400 duration-500 flex items-center gap-2 text-white px-2 py-1 rounded-md">
+                                <LogOut size={16} />
+                            </button>
+                        </div>
                     </div>
-                    <div className="flex items-center">
-                        <div className="text-sm font-semibold">Admin</div>
+                    <div className="h-screen px-4">
+                        <Cache />
+                        <Outlet />
                     </div>
                 </div>
-                <div className="h-screen px-4">
-                    <Cache />
-                    <Outlet />
-                </div>
             </div>
-        </div>
     );
 }
--- src/components/admin/SideBar.jsx
+++ src/components/admin/SideBar.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 const SvgIcon = ({ d }) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
@@ -17,6 +18,7 @@ function ItemOption({ item, activeItem, setActiveItem, toggleSection, openSectio
                     }`}
                 onClick={() => {
                     toggleSection(item.name);
+                    setActiveItem(item.name);
                     if (item.name == "Bảng điều khiển") {
                         navigate('/admin');
                     }
@@ -53,32 +55,36 @@ export default function SideBar() {
         {
             name: 'Bảng điều khiển',
             icon: 'M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z', // Biểu tượng Bảng điều khiển
-            section: 'CHÍNH', subItems: []
+            section: 'CHÍNH', subItems: [],
+            access: ['ADMIN']
         },
         {
             name: 'Khóa học',
             icon: 'm7.875 14.25 1.214 1.942a2.25 2.25 0 0 0 1.908 1.058h2.006c.776 0 1.497-.4 1.908-1.058l1.214-1.942M2.41 9h4.636a2.25 2.25 0 0 1 1.872 1.002l.164.246a2.25 2.25 0 0 0 1.872 1.002h2.092a2.25 2.25 0 0 0 1.872-1.002l.164-.246A2.25 2.25 0 0 1 16.954 9h4.636M2.41 9a2.25 2.25 0 0 0-.16.832V12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 12V9.832c0-.287-.055-.57-.16-.832M2.41 9a2.25 2.25 0 0 1 .382-.632l3.285-3.832a2.25 2.25 0 0 1 1.708-.786h8.43c.657 0 1.281.287 1.709.786l3.284 3.832c.163.19.291.404.382.632M4.5 20.25h15A2.25 2.25 0 0 0 21.75 18v-2.625c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125V18a2.25 2.25 0 0 0 2.25 2.25Z',
             section: 'CHÍNH',
             subItems: [
-                { title: 'Danh sách khóa học', url: '/admin/courses' },
-                { title: 'Thêm khóa học', url: '/admin/add-course' }
-            ]
+                { title: 'Danh sách khóa học', url: './courses' },
+                { title: 'Thêm khóa học', url: './add-course' }
+            ],
+            access: ['ADMIN', 'TEACHER']
         },
         {
             name: 'Giáo viên',
             icon: 'M12 2c2.21 0 4 1.79 4 4s-1.79 4-4 4-4-1.79-4-4 1.79-4 4-4zM6 20c0-3 3-5 6-5s6 2 6 5H6z',
             section: 'CHÍNH', subItems: [
-                { title: 'Danh sách giáo viên', url: '/admin/teachers' },
-                { title: 'Thêm giáo viên', url: '/admin/add-teacher' }
-            ]
+                { title: 'Danh sách giáo viên', url: './teachers' },
+                { title: 'Thêm giáo viên', url: './add-teacher' }
+            ],
+            access: ['ADMIN']
         },
         {
             name: 'Học sinh',
             icon: 'M15 19.128a9.38 9.38 0 0 0 2.625.372 9.337 9.337 0 0 0 4.121-.952 4.125 4.125 0 0 0-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 0 1 8.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0 1 11.964-3.07M12 6.375a3.375 3.375 0 1 1-6.75 0 3.375 3.375 0 0 1 6.75 0Zm8.25 2.25a2.625 2.625 0 1 1-5.25 0 2.625 2.625 0 0 1 5.25 0Z', // Biểu tượng Học sinh
             section: 'CHÍNH',
             subItems: [
-                { title: 'Danh sách học viên', url: '/admin/students' }
-            ]
+                { title: 'Danh sách học viên', url: './students' }
+            ],
+            access: ['ADMIN', 'COLLABORATOR', 'TEACHER']
         },
         ,
         {
@@ -86,37 +92,43 @@ export default function SideBar() {
             icon: 'M4.5 17H4a1 1 0 0 1-1-1 3 3 0 0 1 3-3h1m0-3.05A2.5 2.5 0 1 1 9 5.5M19.5 17h.5a1 1 0 0 0 1-1 3 3 0 0 0-3-3h-1m0-3.05a2.5 2.5 0 1 0-2-4.45m.5 13.5h-7a1 1 0 0 1-1-1 3 3 0 0 1 3-3h3a3 3 0 0 1 3 3 1 1 0 0 1-1 1Zm-1-9.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z',
             section: 'CHÍNH',
             subItems: [
-                { title: 'Danh sách người dùng', url: '/admin/users' },
-                { title: 'Thêm người dùng', url: '/admin/add-user' }
-            ]
+                { title: 'Danh sách người dùng', url: './users' },
+                { title: 'Thêm người dùng', url: './add-user' }
+            ],
+            access: ['ADMIN']
         },
         {
             name: 'Key & Giao dịch',
             icon: 'M12 0C8.686 0 6 2.686 6 6v6H2c-1.104 0-2 .896-2 2v1c0 1.104.896 2 2 2h4v2c0 1.104.896 2 2 2s2-.896 2-2v-2h4c1.104 0 2-.896 2-2v-1c0-1.104-.896-2-2-2h-4V6c0-3.314-2.686-6-6-6zm0 2c2.209 0 4 1.791 4 4v6h-8V6c0-2.209 1.791-4 4-4z', // Biểu tượng Giao dịch
             section: 'CHÍNH',
             subItems: [
-                { title: 'Mã truy cập (Giao dịch)', url: '/admin/access-keys' },
-                { title: 'Tạo mã', url: '/admin/add-key' }
-            ]
+                { title: 'Mã truy cập (Giao dịch)', url: './access-keys' },
+                { title: 'Tạo mã', url: './add-key' }
+            ],
+            access: ['ADMIN', 'COLLABORATOR']
         },
         {
             name: 'Tin tức',
             icon: 'M20 6v14H4V6z', // Biểu tượng Tin tức
             section: 'CHÍNH',
             subItems: [
-                { title: 'Danh sách tin tức', url: '/admin/news' },
-                { title: 'Thêm tin tức', url: '/admin/add-news' }
-            ]
+                { title: 'Danh sách tin tức', url: './news' },
+                { title: 'Thêm tin tức', url: './add-news' }
+            ],
+            access: ['ADMIN']
         },
         {
             name: 'Cài đặt',
             icon: "M11.828 2.25c-.916 0-1.699.663-1.85 1.567l-.091.549a.798.798 0 0 1-.517.608 7.45 7.45 0 0 0-.478.198.798.798 0 0 1-.796-.064l-.453-.324a1.875 1.875 0 0 0-2.416.2l-.243.243a1.875 1.875 0 0 0-.2 2.416l.324.453a.798.798 0 0 1 .064.796 7.448 7.448 0 0 0-.198.478.798.798 0 0 1-.608.517l-.55.092a1.875 1.875 0 0 0-1.566 1.849v.344c0 .916.663 1.699 1.567 1.85l.549.091c.281.047.508.25.608.517.06.162.127.321.198.478a.798.798 0 0 1-.064.796l-.324.453a1.875 1.875 0 0 0 .2 2.416l.243.243c.648.648 1.67.733 2.416.2l.453-.324a.798.798 0 0 1 .796-.064c.157.071.316.137.478.198.267.1.47.327.517.608l.092.55c.15.903.932 1.566 1.849 1.566h.344c.916 0 1.699-.663 1.85-1.567l.091-.549a.798.798 0 0 1 .517-.608 7.52 7.52 0 0 0 .478-.198.798.798 0 0 1 .796.064l.453.324a1.875 1.875 0 0 0 2.416-.2l.243-.243c.648-.648.733-1.67.2-2.416l-.324-.453a.798.798 0 0 1-.064-.796c.071-.157.137-.316.198-.478.1-.267.327-.47.608-.517l.55-.091a1.875 1.875 0 0 0 1.566-1.85v-.344c0-.916-.663-1.699-1.567-1.85l-.549-.091a.798.798 0 0 1-.608-.517 7.507 7.507 0 0 0-.198-.478.798.798 0 0 1 .064-.796l.324-.453a1.875 1.875 0 0 0-.2-2.416l-.243-.243a1.875 1.875 0 0 0-2.416-.2l-.453.324a.798.798 0 0 1-.796.064 7.462 7.462 0 0 0-.478-.198.798.798 0 0 1-.517-.608l-.091-.55a1.875 1.875 0 0 0-1.85-1.566h-.344ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z",
             section: 'KHÁC', subItems: [
-                { title: 'Cài đặt trang web', url: '/admin/settings' },
-                { title: 'Bảo mật', url: '/admin/users' }
-            ]
+                { title: 'Cài đặt trang web', url: './settings' },
+                { title: 'Bảo mật', url: './users' }
+            ],
+            access: ['ADMIN']
         },
     ];
+
+
 
 
     const toggleSection = (name) => {
@@ -144,6 +156,7 @@ export default function SideBar() {
                             {menuItems
                                 .filter((item) => item.section === section)
                                 .map((item) => (
+                                    item.access.includes(Cookies.get('role')) &&
                                     <ItemOption
                                         key={item.name}
                                         item={item}
