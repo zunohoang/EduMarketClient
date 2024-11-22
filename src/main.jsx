015ce50221f9e73cc6f8acceb365f0ffa7a90171
zunohoang <nguyenvanhoang2005nt@gmail.com>
2024-11-22 08:04:32 +0700

    FIX: localstorage -> cookies

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => { };
+  console.debug = () => { };
+  console.warn = () => { };
+  console.error = () => { };
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
